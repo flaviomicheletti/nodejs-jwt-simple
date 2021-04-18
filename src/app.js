55ec09b978e7c7df350c5364c8eb534df5a0f283
Flávio Micheletti <flaviomicheletti@hotmail.com>
2021-04-18 16:31:59 -0300

    detalhes

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const cors = require('cors');
-const morgan = require('morgan');
+const logger = require('morgan');
 const routers = require('./routers')
 
 const app = express();
 
-app.use(morgan('dev'));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(cors());
 app.use(routers());
